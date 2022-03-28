ea679e4db6ab0afb167db0a0783eecdab52a5651
AminurAlam <oscorp2004@gmail.com>
2022-03-28 22:18:05 +0530

    more js

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,15 @@
 multi line comment
 */
 
+
+
+
+
+
+
+//outputting to console
 console.log("hello world")
+console.log(`1 + 2 is ${1+2}`)
 
 
 //assigning values to a variable
@@ -55,4 +63,30 @@ function findLength(data) {
   console.log(data, "is", data.length, "characters long")
 }
 
-findLength("hello world!");
+function add(val1,val2) {
+  return val1 + val2
+}
+
+function print(data) {
+  console.log(data)
+}
+
+//calling functions
+findLength("fish")
+let sum = add(10,30)
+print(sum)
+
+console.log(`${string} is ${num}`)
+
+
+//launching html pages on localhost
+const express = require("express") //importing
+const app = express()
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, function () {
+    console.log("Server is running on 127.0.0.1:3000")
+})
