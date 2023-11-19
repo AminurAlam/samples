bf3cd78543bba5fba2b75780161d2582513b7bb1
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2023-11-19 22:37:06 +0530

    c: avg, bin2dec, palindrome

--- c/main.c
+++ c/main.c
@@ -87,13 +87,73 @@ void swap(int a, int b) {
     printf("a = %d, b = %d\n", a, b);
 }
 
+// check if a number is palindrome or not
+void palindrome() {
+    int num, num_copy, num_reverse = 0;
+    printf("enter an integer: ");
+    scanf("%d", &num);
+    num_copy = num;
+
+    do {
+        num_reverse = (num_reverse * 10) + (num % 10);
+        num /= 10;
+    } while (num != 0);
+
+    if (num_copy == num_reverse) 
+        printf("%d is a palindrome\n", num_copy);
+    else 
+        printf("%d is not a palindrome\n", num_copy);
+
+}
+
+// convert binary number to decimal number
+void bin_to_dec() {
+    long num, rem, dec = 0;
+    printf("enter number: ");
+    scanf("%ld", &num);
+
+    for (int i = 0; num != 0; i++) {
+        rem = num % 10;
+        num /= 10;
+        dec += rem * pow(2, i);
+    }
+
+    printf("%ld\n", dec);
+}
+
+// find average of marks
+void avg() {
+    float avg, entry = 0, count = 0, sum = 0;
+    printf("enter score below (-ve to end)\n");
+
+    // while (0 <= entry) {
+    //     scanf("%f", &entry);
+    //     sum += entry;
+    //     count++;
+    // }
+
+    for (entry = 0; 0 <= entry; count++) {
+        scanf("%f", &entry);
+        sum += entry;
+    }
+
+    avg = (sum - entry) / (count - 1);
+    printf("average is: %.2f\n", avg);
+}
+
 int main() {
     // simple_intrest();
+    // compound_intrest();
     // get_ascii();
     // get_rectangle_area();
     // get_square_area();
-    for (int i = 1; i <= 10; i++) { odd_even(i); }
-    prime();
-    swap(10, 12);
+    // get_circle_area();
+    // prime();
+    // for (int i = 1; i <= 10; i++) { odd_even(i); }
+    // swap(10, 12);
+    palindrome();
+    // palindrome_str();
+    // bin_to_dec();
+    // avg();
     return 0;
 }
