bdbfe6edfc16e9b3f35ff09f90807c8cb5f34fe2
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-03-07 12:53:04 +0530

    c: improve queue

--- c/queue.c
+++ c/queue.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#define MAX 10
+#define MAX 5
 
 int A[MAX], f = -1, r = -1;
 
 void display() {
     if (f == -1) {
-        printf("empty\n");
+        printf("[ ]\n");
         return;
     }
 
     printf("[ ");
-    for (int i = 0; i < r; i++) {
-        printf("%d", A[i]);
-        printf(", ");
-    }
-    printf("%d ]\n", A[r]);
+    for (int i = f; i < r; i++) { printf("\033[33m%d\033[0m, ", A[i]); }
+    printf("\033[33m%d\033[0m ]\n", A[r]);
 }
 
-void insert(int item) {
+void insert() {
+    int item;
     if (r == MAX - 1) {
         printf("overflow!\n");
     } else {
+        printf("insert: ");
+        scanf("%d", &item);
         A[++r] = item;
         if (f == -1) f++;
     }
@@ -28,32 +28,35 @@ void insert(int item) {
 
 void delete() {
     if (f == -1) {
-        printf("underflow\n");
-    } else if (f == r--) {
-        r = -1;
+        printf("underflow!\n");
+    } else if (r == f++) {
+        printf("removed: %d\n", A[f - 1]);
+        printf("reset queue!\n");
         f = -1;
+        r = -1;
+    } else {
+        printf("removed: %d\n", A[f - 1]);
     }
 }
 
 int main() {
-    int item, choice;
+    int choice;
 
-    printf("choose:\n");
-    printf("1 - insert\n");
-    printf("2 - delete\n");
-    printf("3 - exit\n");
+    printf("initialized array of size %d\n", MAX);
+    printf("1: insert; 2: delete; 3: reset; 4: debug; *: exit\n");
 
     while (1) {
-        printf(" > ");
+        printf("\n > ");
         scanf("%d", &choice);
-
         switch (choice) {
-        case 1:
-            scanf("%d", &item);
-            insert(item);
+        case 1: insert(); break;
+        case 2: delete (); break;
+        case 3:
+            f = -1;
+            r = -1;
             break;
-        case 2: delete(); break;
-        default: return 1; break;
+        case 4: printf("front: %d; back: %d\n", f, r); break;
+        default: return 0; break;
         }
         display();
     }
