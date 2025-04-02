ddbe611f24295fc538d7046e558790473f6ce781
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-04-02 22:05:58 +0530

    rust: doing some exercises

--- rust/main.rs
+++ rust/main.rs
@@ -19,24 +19,44 @@ fn main() {
     */
 
     // Variables can be type annotated.
-    let a_float: f64 = 1.0;  // Regular annotation
-    let an_integer   = 5i32; // Suffix annotation
+    let _float: f64 = 1.0; // Regular annotation
+    let _integer = 5i32; // Suffix annotation
 
     // Or a default will be used.
-    let default_float   = 3.0; // `f64`
-    let default_integer = 7;   // `i32`
+    let _float = 3.0; // `f64`
+    let _integer = 7; // `i32`
 
     // A type can also be inferred from context
-    let mut inferred_type = 12; // Type i64 is inferred from another line
-    inferred_type = 4294967296i64;
+    let mut _inferred_type = 12; // Type i64 is inferred from another line
+    _inferred_type = 4294967296i64;
 
     // A mutable variable's value can be changed.
-    let mut mutable = 12; // Mutable `i32`
-    mutable = 21;
+    let mut _mutable = 12; // Mutable `i32`
+    _mutable = 21;
 
     // Error! The type of a variable can't be changed.
     // mutable = true;
 
     // Variables can be overwritten with shadowing.
-    let mutable = true;
+    let _mutable = true;
+
+    print!("hello world!\n");
+    print!(
+        "
+    i am printing
+    multiple lines
+    \n"
+    );
+
+    // check type
+    fn type_of<T>(_: &T) {
+        println!("{}", std::any::type_name::<T>())
+    }
+    type_of(&"string");
+    type_of(&'c');
+    type_of(&10.10);
+    type_of(&true);
+
+    // vector
+    vec![1, 2, 3];
 }
