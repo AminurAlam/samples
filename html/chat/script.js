47b2dcdb9e02767dc36758e9ed3711041a37a4b0
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-05-01 11:53:18 +0530

    html: popup, scroll & size

--- html/chat/script.js
+++ html/chat/script.js
@@ -1,22 +1,3 @@
-function avg(words) {
-  var sum = (words || [])
-    .map((word) => word.match(/\w+/)[0].length)
-    .reduce((acc, curr) => acc + curr, 0);
-  return sum / (words || "1").length;
-}
-
-function ratio(a, b) {
-  if (!a || !b) {
-    return `${a}:${b}`;
-  }
-  var hcf = 1;
-  var max = a > b ? a : b;
-  for (i = 2; i <= max; i++) {
-    if (a % i == 0 && b % i == 0) var hcf = i;
-  }
-  return `${a / hcf}:${b / hcf}`;
-}
-
 addEventListener("input", (event) => {
   var txt = buf.value;
   var words = txt.match(/\w+(\W|$)/g);
@@ -25,7 +6,6 @@ addEventListener("input", (event) => {
   alnum.innerText = (txt.match(/\w/g) || "").length;
   word.innerText = (words || "").length;
   vovl.innerText = (txt.match(/[aeiou]/g) || "").length;
-  cpw.innerText = avg(words);
   if (txt.length == 0) {
     sen.innerText = 0;
   } else {
@@ -34,8 +14,4 @@ addEventListener("input", (event) => {
     }).length;
   }
   para.innerText = txt.split("\n\n").filter((str) => str).length;
-  // vtoc.innerText = ratio(
-  //   (txt.match(/[aeiou]/g) || "").length,
-  //   (txt.match(/[b-df-hj-np-tv-z]/g) || "").length,
-  // );
 });
