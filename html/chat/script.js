6c349d8d716dad08c938008cf4da798178f62121
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-05-01 08:14:59 +0530

    html

--- html/chat/script.js
+++ html/chat/script.js
@@ -22,11 +22,20 @@ addEventListener("input", (event) => {
   var words = txt.match(/\w+(\W|$)/g);
 
   char.innerText = txt.length;
+  alnum.innerText = (txt.match(/\w/g) || "").length;
   word.innerText = (words || "").length;
   vovl.innerText = (txt.match(/[aeiou]/g) || "").length;
   cpw.innerText = avg(words);
-  vtoc.innerText = ratio(
-    (txt.match(/[aeiou]/g) || "").length,
-    (txt.match(/[b-df-hj-np-tv-z]/g) || "").length,
-  );
+  if (txt.length == 0) {
+    sen.innerText = 0;
+  } else {
+    sen.innerText = txt.split(/[\.\?!\n]/).filter((sentence) => {
+      return sentence && !sentence.match(/[\.\?!\n]/);
+    }).length;
+  }
+  para.innerText = txt.split("\n\n").filter((str) => str).length;
+  // vtoc.innerText = ratio(
+  //   (txt.match(/[aeiou]/g) || "").length,
+  //   (txt.match(/[b-df-hj-np-tv-z]/g) || "").length,
+  // );
 });
