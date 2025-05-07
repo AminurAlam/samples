a26a87784032ae57d262a990bedee350180cdede
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-05-07 17:23:29 +0530

    html: minify

--- html/chat/script.js
+++ html/chat/script.js
@@ -1,17 +1,10 @@
-addEventListener("input", (event) => {
+addEventListener("input", () => {
   var txt = buf.value;
-  var words = txt.match(/\w+(\W|$)/g);
 
   char.innerText = txt.length;
-  alnum.innerText = (txt.match(/\w/g) || "").length;
-  word.innerText = (words || "").length;
-  vovl.innerText = (txt.match(/[aeiou]/g) || "").length;
-  if (txt.length == 0) {
-    sen.innerText = 0;
-  } else {
-    sen.innerText = txt.split(/[\.\?!\n]/).filter((sentence) => {
-      return sentence && !sentence.match(/[\.\?!\n]/);
-    }).length;
-  }
-  para.innerText = txt.split("\n\n").filter((str) => str).length;
+  word.innerText = (txt.match(/\w+(\W|$)/g) || "").length;
+  sen.innerText = txt.split(/[\.\?!\n]/).filter((sentence) => {
+    return sentence && !sentence.match(/[\.\?!\n]/);
+  }).length;
+  para.innerText = txt.split(/\n\n+/).filter((str) => str).length;
 });
