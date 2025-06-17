f34c4ebc85d3ec37d50ee1c06ea601d104babf8e
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-06-17 18:08:37 +0530

    lua: format

--- lua/app.lua
+++ lua/app.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua
 
---single line comment
+-- single line comment
 
 --[[
     multi line comment
@@ -11,18 +11,14 @@
 ]][[]]][]]
 --]=]
 
-
-
---[[ printing something ]]--
-print("hello world!")
+--[[ printing something ]]
+print('hello world!')
 print([[
     i am printing
     multiple lines
 ]])
-io.write("another way to print\n")
-print(string.format("sum of 1 and 2 is %d",1+2))
-
-
+io.write('another way to print\n')
+print(string.format('sum of 1 and 2 is %d', 1 + 2))
 
 
 --[[data types]]
@@ -34,14 +30,14 @@ print(type(true))          --> boolean
 print(type(nil))           --> nil
 
 -- converting string to number
-print(tonumber("10"))
-print("10" + 2) --> 12
+print(tonumber('10'))
+print('10' + 2) --> 12
+
 -- converting number to string
 print(tostring(17))
-print(10 .. "5") --> 105
+print(10 .. '5') --> 105
 
-
---[[ variables ]]--
+--[[ variables ]]
 --[[ The following words are reserved
 we cannot use them as identifiers:
     and       break  end    in      if  do
@@ -53,39 +49,36 @@ we cannot use them as identifiers:
 -- declaring variables
 
 a = 1
-b = a*2
+b = a * 2
 
-a = 1 b = a*2
+a = 1 b = a * 2
 
-local text = "example text"  -- local variable
-TEXT = "another text"  -- global variable
+local text = 'example text' -- local variable
+TEXT = 'another text' -- global variable
 
 _ = nil -- dummy variable
 
-local word = "lorem"
-local num = 21  -- number
+local word = 'lorem'
+local num = 21 -- number
 local bool = true
 local nothing = nil
 
-
-
 -- printing variables
 print(text, TEXT)
-print(_VERSION)  -- predefined variable
+print(_VERSION) -- predefined variable
 
 -- deleting variable
 text = nil
 TEXT = nil
 
-
---[[ tables ]]--
+--[[ tables ]]
 
 -- creating table
 array = {
-    "example",
-    10,
-    text,
-    ['key'] = 'value'
+  'example',
+  10,
+  text,
+  ['key'] = 'value',
 }
 
 print(array[1])     --> "example"
@@ -94,33 +87,27 @@ print(array['key']) --> "value"
 print(array.key)    --> value
 
 -- adding item
-array['user'] = "user_name"
+array['user'] = 'user_name'
 array.new_item = true
 
 -- removing item
 -- accessing item
 
-
-
-
---[[ functions ]]--
+--[[ functions ]]
 
 -- creating
-function add(n1, n2)
-    print(n1+n2)
-end
-function factorial(n)
-    if n == 0 then
-        return 1
-    else
-        return n * factorial(n-1)
-    end
+local add = function(n1, n2) print(n1 + n2) end
+local factorial = function(n)
+  if n == 0 then
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
 end
 
 -- calling functions
 add(5, 7)
 print(factorial(4))
 
-
-
---[[ loops ]]--
+--[[ loops ]]
+-- TODO: loops
